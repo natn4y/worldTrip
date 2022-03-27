44cec410e86b20a4bf172206a3841cdad0b08d1e
Natanael Araújo <natanael@hotmail.co.jp>
2022-03-27 19:08:26 -0300

    📂 Componente ContinentBanner

--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -1,10 +1,23 @@
 import { Flex } from "@chakra-ui/react";
 import { Header } from "../../components/Header";
+import { ContinentBanner } from "../../components/ContinentBanner";
+import { Content } from "../../components/Content";
 
 export default function Continent() {
   return (
     <Flex direction="column">
       <Header />
+      <ContinentBanner />
+
+      <Flex
+        direction="column"
+        maxW="1160px"
+        px="1rem"
+        mx="auto"
+        mb="10"
+      >
+        <Content />
+      </Flex>
     </Flex>
   );
 }
