f91a18db1a8e6ab2ddaebd0d51486f6d30fd70f5
Natanael Araújo <natanael@hotmail.co.jp>
2022-03-26 23:21:40 -0300

    ⭐ 🔧 Configuração inicial do chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../styles/theme";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
-  )
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default App;
