76a690033825dff7efd797558ae775e05e5ae00a
Natanael Araújo <natanael@hotmail.co.jp>
2022-03-27 03:33:56 -0300

    ( Route: / ): Texto depois do separator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { Header } from "../components/Header";
 import { Banner } from "../components/Banner/index";
@@ -14,6 +14,15 @@ const Home: NextPage = () => {
       <Banner />
       <Caracteristicas />
       <Separator />
+
+      <Heading
+        textAlign="center"
+        fontWeight="500"
+        mb={['5', 14]}
+        fontSize={['lg', '3xl', '4x1']}
+      >
+        Vamos nessa? <br />Então escolha seu continente
+      </Heading>
     </>
   );
 };
